3f24e5db2bb4bbc32755da85c37d410db14f6223
AndresFAD <andresaldur99@gmail.com>
2024-11-07 21:59:35 -0600

    Update Save Product Function

--- frontend/src/components/ui/ModalProduct.tsx
+++ frontend/src/components/ui/ModalProduct.tsx
@@ -41,12 +41,11 @@ export default function ModalProduct({ product, setShowModal }: props) {
     const handleSave = () => {
         console.log("enter to handleSave")
         console.log(actualProduct)
-        toast.success("Product has been updated!", {
-            position: "bottom-right",
-            autoClose: 2000,
-
-        })
         setShowModal(false)
+        toast.success("Employee has been updated!", {
+            position: "top-right",
+            autoClose: 2000,
+        });
         if (validateForm()) {
             axios.post('https://api.example.com/data', product)
                 .then(() =>
@@ -91,19 +90,19 @@ export default function ModalProduct({ product, setShowModal }: props) {
                         <div className="mx-auto block w-10/12 rounded-lg bg-white p-6 shadow-4 dark:bg-surface-dark">
                             <div className="mb-5">
                                 <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                                <input value={actualProduct.title ? actualProduct.title : ""} type="text" id="title" onChange={(e) => handleChangeFiled("title", e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title"  />
+                                <input value={actualProduct.title ? actualProduct.title : ""} type="text" id="title" onChange={(e) => handleChangeFiled("title", e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title" />
                             </div>
                             <div className="mb-5">
                                 <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                                <input type="text" value={actualProduct.description ? actualProduct.description : ""} onChange={(e) => handleChangeFiled("description", e.target.value)} placeholder="Description" id="decription" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  />
+                                <input type="text" value={actualProduct.description ? actualProduct.description : ""} onChange={(e) => handleChangeFiled("description", e.target.value)} placeholder="Description" id="decription" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                             </div>
                             <div className="mb-5">
                                 <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
-                                <input type="text" value={actualProduct.price ? actualProduct.price : ""} onChange={(e) => handleChangeFiled("price", e.target.value)} placeholder="Price" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  />
+                                <input type="text" value={actualProduct.price ? actualProduct.price : ""} onChange={(e) => handleChangeFiled("price", e.target.value)} placeholder="Price" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                             </div>
                             <div className="mb-5">
                                 <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
-                                <input placeholder="Category" value={actualProduct.category ? actualProduct.category : ""} onChange={(e) => handleChangeFiled("category", e.target.value)} type="text" id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  />
+                                <input placeholder="Category" value={actualProduct.category ? actualProduct.category : ""} onChange={(e) => handleChangeFiled("category", e.target.value)} type="text" id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                             </div>
 
                             <div className="mb-5">
@@ -122,7 +121,7 @@ export default function ModalProduct({ product, setShowModal }: props) {
                                 </button>
                                 <button
                                     className="bg-indigo-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                    onClick={() => handleSave} >
+                                    onClick={handleSave} >
                                     {actualProduct.id ? (<i className='bx bx-edit'>Update Product</i>) : (<i className='bx bx-message-alt-add'>Create Product</i>)}
                                 </button>
 
